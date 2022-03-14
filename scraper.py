5877b697e93a5e16bf11aaff3676c68cbc233f24
Emmet Ryan <emmetryan7@gmail.com>
2022-03-14 12:26:22 -0400

    Removed outdated code

--- scraper.py
+++ scraper.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located
-from selenium.webdriver.common.by import By
 import os
-timeout=5
 
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
@@ -20,7 +16,6 @@ op.add_argument(f'user-agent={user_agent}')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 
 driver.get(url)
-WebDriverWait(driver, 60).until(visibility_of_element_located((By.ID, 'downloadData')))
 download1 = driver.find_element_by_id('downloadData')
 download1.click()
 
